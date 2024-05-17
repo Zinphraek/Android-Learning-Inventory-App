3a336a56879a7bbeb959517e829004308548dea5
zinphraek <zinphraek@gmail.com>
2024-05-17 00:36:16 -0400

    Updated dependencies version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,9 +79,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     //Room
-    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
